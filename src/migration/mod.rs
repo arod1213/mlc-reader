70cc1767d05aae40388a784a91546cbb969a9f04
arod1213 <aidan@aidanrodriguez.com>
2026-07-13 08:53:37 -0700

    update role setting sql

--- src/migration/mod.rs
+++ src/migration/mod.rs
@@ -75,33 +75,29 @@ pub async fn create_indexes(conn: &Connection) -> Result<(), libsql::Error> {
 /// indicate whether a party is primarily a writer / publisher
 pub async fn assign_roles(conn: &libsql::Transaction) -> Result<(), libsql::Error> {
     let sql = "
-      UPDATE parties
-      SET role = CASE
-        WHEN (
-          SELECT COUNT(*)
+        WITH writer_occurrences AS (
+          SELECT COUNT(*) as count
           FROM publisher_relations
           WHERE child_id = parties.id
-        ) > (
-          SELECT COUNT(*)
+        ),
+        publisher_occurrences AS (
+          SELECT COUNT(*) as count
           FROM publisher_relations
           WHERE parent_id = parties.id
-        ) THEN 'writer'
-        WHEN (
-          SELECT COUNT(*)
-          FROM publisher_relations
-          WHERE parent_id = parties.id
-        ) > (
-          SELECT COUNT(*)
-          FROM publisher_relations
-          WHERE child_id = parties.id
-        ) THEN 'publisher'
-        WHEN (
-          SELECT COUNT(*)
-          FROM publisher_relations
-          WHERE parent_id = parties.id
-        ) > 0 THEN 'both'
-        ELSE NULL
-      END;
+        )
+        UPDATE parties
+        SET role = CASE
+            WHEN 
+                ((SELECT count FROM writer_occurrences) > (SELECT count FROM publisher_occurrences))
+            THEN 'writer'
+            WHEN 
+                ((SELECT count FROM writer_occurrences) < (SELECT count FROM publisher_occurrences))
+            THEN 'publisher'
+            WHEN 
+                ((SELECT count FROM writer_occurrences) = (SELECT count FROM publisher_occurrences))
+            THEN 'both'
+            ELSE NULL
+        END;
         ";
     _ = conn.execute(sql, params!()).await?;
     Ok(())
