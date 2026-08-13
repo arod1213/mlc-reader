fdfc96e3eaea160e85bd46a56360fa553b43eb8f
arod1213 <aidan@aidanrodriguez.com>
2026-08-13 08:37:06 -0700

    add full pipeline info

--- pipeline.sh
+++ pipeline.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 #DATA_PATH="./Sample Data"
 DATA_PATH="./mlc-bwarm"
 
+export SQLITE_TMPDIR="./sqlite-tmp"
+export TMPDIR=$SQLITE_TMPDIR
+export RUST_LOG=debug
+
 cargo build --release
 BIN="./target/release/mlc-reader"
 
@@ -12,9 +16,7 @@ $BIN save -p "$DATA_PATH"
 $BIN migrate -p "$DATA_PATH"
 $BIN trim # --vacuum
 $BIN index-search
-
-#$BIN enrich -m writer > /dev/null
-#$BIN enrich -m publisher > /dev/null
-#$BIN enrich -m share > /dev/null 
-
-#$BIN update -p './updates.txt' && \
+$BIN enrich -m writer > /dev/null
+$BIN enrich -m publisher > /dev/null
+$BIN enrich -m share > /dev/null 
+$BIN update -p './updates.txt'
