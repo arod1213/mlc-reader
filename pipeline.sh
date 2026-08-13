6d634f02e55759fb6339f46aef71762a1eeafa69
arod1213 <aidan@aidanrodriguez.com>
2026-08-13 09:16:41 -0700

    setup proper tmp dir stuff

--- pipeline.sh
+++ pipeline.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#DATA_PATH="./mlc-sample"
-#DATA_PATH="./Sample Data"
-DATA_PATH="./mlc-bwarm"
-
 export SQLITE_TMPDIR="./sqlite-tmp"
 export TMPDIR=$SQLITE_TMPDIR
 export RUST_LOG=debug
 
+mkdir -p $SQLITE_TMPDIR
+chmod 700 $SQLITE_TMPDIR
+
+#TSV_STORAGE_PATH="./mlc-sample"
+#TSV_STORAGE_PATH="./Sample Data"
+TSV_STORAGE_PATH="./mlc-bwarm"
+
 cargo build --release
 BIN="./target/release/mlc-reader"
 
-$BIN save -p "$DATA_PATH"
-$BIN migrate -p "$DATA_PATH"
+$BIN save -p "$TSV_STORAGE_PATH"
+$BIN migrate -p "$TSV_STORAGE_PATH"
 $BIN trim # --vacuum
 $BIN index-search
 $BIN enrich -m writer > /dev/null
